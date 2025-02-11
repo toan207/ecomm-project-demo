6cde0805326f6e3077c91480314f112ae04e8e92
toan-wins <buiductoan207@gmail.com>
2025-02-11 23:37:29 +0700

    cached category tree

--- cron.js
+++ cron.js
@@ -1,6 +1,7 @@
-const { loadCachedCategory } = require('./lib/category.lib');
+const { loadCachedCategory, loadCachedCategoryAdmin } = require('./lib/category.lib');
 const schedule = require('node-schedule');
 
 schedule.scheduleJob('* * * * *', () => {
     loadCachedCategory();
+    loadCachedCategoryAdmin();
 });
