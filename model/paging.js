e9ecc37f506a1755746544b7cd13229c379e3971
toan-wins <buiductoan207@gmail.com>
2025-02-12 23:40:40 +0700

    init paging schema

--- model/paging.js
+++ model/paging.js
@@ -1,4 +1,6 @@
 const Paging = (page, limit, totalData) => {
+    if (page == null) page = 1;
+    if (limit == null) limit = 10;
     const skip = (page - 1) * limit;
     const totalPage = Math.ceil(totalData / limit);
     return { page, skip, limit, totalPage };
